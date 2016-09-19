53310504c953cec3c04e00ce54826dc8784a79ae
Garrett Tan <garrett.t.tan@gmail.com>
2016-09-19 15:11:42 -0700

    submitting hw1

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,15 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    sum = 0
+    seen = []
+    a.each_index do |i|
+      x = a[i].to_i + 2
+      if x % 2 == 0 and not seen.include?(x) and x <= 10
+        sum += x
+        seen << x
+      end
+    end
+    return sum
   end
 end
-
-
